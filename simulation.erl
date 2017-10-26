079696b997b488dab36c38a76f6ad5010c3bb1b7
Benjamin Mizera <mizerb@rpi.edu>
2017-10-26 03:02:08 -0400

    Added the timestamp sending and revolting to simulation. Hopefully it will work as desired

--- simulation.erl
+++ simulation.erl
@@ -7,10 +7,10 @@ run(Filename) ->
     %io:format("~w~n",[Data]),
     %NodeData = lists:keyfind("node1",3, Data),
     %io:format("~w~n", [NodeData]),
-    [Left, Center, Right] = getInfo(Data, TestName, length(Data)),
-    io:format("~w~n", [Left]),
-    io:format("~w~n", [Center]),
-    io:format("~w~n", [Right]).
+    [Left, Center, Right] = getInfo(Data, TestName, length(Data)).
+    %io:format("~w~n", [Left]),
+    %io:format("~w~n", [Center]),
+    %io:format("~w~n", [Right]).
 
 
 
@@ -34,3 +34,74 @@ getInfo(Data, Loc, Max) ->
     [Left, Center, Right].
 
 
+master(Info, Time) ->
+    receive
+        %Begin a vote
+        {voteStart, Time} ->
+            io:format("the vote will begin"),
+            master(Info, Time);
+        {voteWin, From} ->
+            From ! {startClock, Time},
+            master(Info, Time+1)
+    end.
+
+                    
+
+add(Revolters, Peasant, Time) ->
+    case lists:member(Peasant, Revolters) of
+        false ->
+            io:format("ID=~w revolted at t=~w~n", [getID(Peasant),Time]),
+            [Peasant | Revolters];
+        true ->
+            Revolters
+    end.
+
+
+deposeCheck(Leader, Myself, RevCount, Max) ->
+    (Leader == Myself) andalso (RevCount >= ((Max+1) div 2) ).
+
+revoltCheck(Leader, Myself, Revolted, Time, Start) ->
+    (Leader /= Myself) and (Revolted == false) and ((Time - Start) > getTolerance(Myself)).
+
+getTolerance({_,_,_,_,Tolerance}) -> Tolerance.
+getID({ID,_,_,_,_}) -> ID.
+
+
+nodelife(Data) ->
+    {Left, Center, Right, Master, Total, Living, Revolted} = Data,
+    %Left is node to Left, Center is self, Right is node to right, Master
+    % is master node, that prints stuff, & Living is if this node has been
+    % the leader before
+    receive
+        {time, Leader, Start, Time, RevCount} ->
+            %Check if Leader and Deposition possible
+            case deposeCheck(Leader, Center, RevCount, Total) of true->
+                writeOut("ID=~w was deposed at t=~w~n",[getID(Center),Time]),
+                Master ! {voteStart, Time},
+                nodelife({Left, Center, Right, Master, Total, false, false})
+            end,
+            %Check if this node revolts
+            case revoltCheck(Leader, Center, Revolted, Time, Start) of true ->
+                writeOut("ID=~w revolted at t=~w~n",[getID(Center),Time]),
+                Revolted = true,
+                RevCount = RevCount + 1
+            end,
+            %send along to next node and hit recursion
+            Left ! {time, Leader, Start, Time + 1, RevCount},
+            nodelife({Left, Center, Right, Master, Total, Living, Revolted});
+        {voteStart} ->
+
+            nodelife({Left, Center, Right, Master, Total, Living, false});
+        {startClock, Time} ->
+            writeOut("ID=~w became leader at t=~w~n",[getID(Center),Time]),
+            Left ! {time, Center, Time, Time+1, 0},
+            nodelife({Left, Center, Right, Master, Total, false, false})
+    end.
+
+
+
+
+
+
+writeOut(Written, Data) ->
+    io:format(Written,Data).
