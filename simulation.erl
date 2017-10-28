ab0cd298a7cf8ba5db6686d2b9f379f8e10a3cdb
Ben Mizera <mizerb@rpi.edu>
2017-10-28 15:13:04 -0400

    idk what I'm doing

--- simulation.erl
+++ simulation.erl
@@ -17,21 +17,18 @@ run(Filename) ->
     %io:format("~w~n", [Right]).
 
 
-create_Actors(0) -> [];
 create_Actors(N) ->
-    Input = {1,1,1,1,1,1,1},
-    [spawn(simulation,nodelife,Input) | create_Actors(Data, N-1)];
+    if N == 0 -> [] end,
+    Input = {1,1,1,1,1},
+    [spawn(simulation,nodelife,Input) | create_Actors( N-1)].
 
-
-addPid({ID,IP,Name,Priority,Tolerance}, PID) ->
-    {ID, IP, Name, Priority, Tolerance, PID}.
 
 getInfo(Data, Pids, Loc, Max) ->
     %this would be so much easier in C
     % I want the damn index of the tuple that matches the name
     Center = lists:keyfind(Loc,3, Data),
     {Index,_} = string:to_integer(string:substr(Loc, 5)),
-    if 
+    if
         Index == 1 ->
             Left = lists:nth(Max,Data),
             Right = lists:nth(Index+1,Data);
@@ -74,9 +71,9 @@ deposeCheck(Leader, Myself, RevCount, Max) ->
 revoltCheck(Leader, Myself, Revolted, Time, Start) ->
     (Leader /= Myself) and (Revolted == false) and ((Time - Start) > getTolerance(Myself)).
 
-getTolerance({_,_,_,_,Tolerance,_}) -> Tolerance.
-getID({ID,_,_,_,_,_}) -> ID.
-getPid({_,_,_,_,_,PID}) -> PID.
+getTolerance({_,_,_,_,Tolerance}) -> Tolerance.
+getID({ID,_,_,_,_}) -> ID.
+
 
 nodelife(Data) ->
     {Left, Center, Right, Master, Total, Living, Revolted} = Data,
@@ -98,17 +95,17 @@ nodelife(Data) ->
                 RevCount = RevCount + 1
             end,
             %send along to next node and hit recursion
-            getPid(Left) ! {time, Leader, Start, Time + 1, RevCount},
+            Left ! {time, Leader, Start, Time + 1, RevCount},
             nodelife({Left, Center, Right, Master, Total, Living, Revolted});
         {voteStart} ->
 
             nodelife({Left, Center, Right, Master, Total, Living, false});
         {startClock, Time} ->
             writeOut("ID=~w became leader at t=~w~n",[getID(Center),Time]),
-            getPid(Left) ! {time, Center, Time, Time+1, 0},
+            Left ! {time, Center, Time, Time+1, 0},
             nodelife({Left, Center, Right, Master, Total, false, false});
-        {orient, {Zleft,ZCenter,Zright, ZMaster, ZTotal}}
-            nodelife({Zleft, ZCenter, Zright, ZMaster,ZTotal, false,false});
+        {orient, {Zleft,ZCenter,Zright, ZMaster, ZTotal}} ->
+            nodelife({Zleft, ZCenter, Zright, ZMaster,ZTotal, false,false})
     end.
 
 
